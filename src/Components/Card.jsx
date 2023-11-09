272a7022e946807cc452ddd3f04809269f36dded
Luciano Kohler da Silva <lucianokohlerdasilva@gmail.com>
2023-11-09 17:17:22 -0300

    adicionado abreviatura de textos se for grande demais

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,5 @@
-
-
 export default function Card(props) {
+
     //Formatando o preço
     const precoFormatado = (props.price).toLocaleString("pt-BR", {
         style: "currency",
@@ -8,13 +7,13 @@ export default function Card(props) {
     });
 
     return (
-        <div className="bg-white w-[300px] h-[500px] rounded-lg p-3">
+        <div className="bg-white w-[300px] h-[460px] rounded-lg p-3">
             <img
                 className="rounded-lg bg-cover bg-center w-[100%]"
                 alt="nada"
                 src={props.image.replace(/\w\.jpg/gi, "W.jpg")}
             />
-            <h3 className="text-lg p-1 h-[100px]">{props.title}</h3>
+            <h3 className="titleAbbreviator text-lg p-1 h-[60px]">{props.title}</h3>
             <h3 className="font-bold text-2xl p-1">{precoFormatado}</h3>
             <a href={props.permalink}>
                 <button className="bg-green-500 text-white font-bold px-4 py-2 w-[75%] rounded-lg my-2 mx-auto block">
